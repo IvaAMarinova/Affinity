1f6ce8e9425d36354d08979625a12abc22b2dd72
Iva Marinova <81924785+IvaAMarinova@users.noreply.github.com>
2022-04-02 11:09:59 +0300

    moved everything
    moved everything I had only on my machine to git.
    
    commented functions aren't setup yet.

---
+++ object_recognition.py
@@ -0,0 +1,49 @@
+import cv2 
+import uuid
+import os
+import time
+import tensorflow as tf
+print(tf.version.VERSION)
+
+print('hi')
+labels = ['thumbsup', 'thumbsdown', 'highfive', 'halfheart', 'fuckyou']
+number_imgs = 5
+
+IMAGES_PATH = os.path.join('Tensorflow', 'workspace', 'images', 'collectedimages')
+#print(IMAGES_PATH)
+
+if not os.path.exists(IMAGES_PATH):
+    if os.name == 'posix':
+        os.makedirs(IMAGES_PATH)
+    elif os.name == 'nt':
+        os.makedirs(IMAGES_PATH)
+
+for label in labels:
+    path = os.path.join(IMAGES_PATH, label)
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+for label in labels:
+    cap = cv2.VideoCapture(0)
+    print('Collecting images for {}'.format(label))
+    time.sleep(5)
+    for imgnum in range(number_imgs):
+        print('Collecting image {}'.format(imgnum))
+        ret, frame = cap.read()
+        imgname = os.path.join(IMAGES_PATH,label,label+'.'+'{}.jpg'.format(str(uuid.uuid1())))
+        cv2.imwrite(imgname, frame)
+        cv2.imshow('frame', frame)
+        time.sleep(2)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
+
+
+LABELIMG_PATH = os.path.join('Tensorflow', 'labelimg')
+
+if not os.path.exists(LABELIMG_PATH):
+    os.makedirs(LABELIMG_PATH)
+   # !git clone https://github.com/tzutalin/labelImg {LABELIMG_PATH}
+
---
+++ random_functions.py
@@ -0,0 +1,191 @@
+import pyttsx3  
+import speech_recognition as sr
+import datetime
+#import smtplib
+import wikipedia
+import webbrowser as wb
+import pywhatkit  as kit
+from newsapi import NewsApiClient
+import clipboard
+import os
+import pyjokes
+import pyautogui
+import time as tt
+#import spotipy
+import json
+#import requests
+#import wolframalpha 
+
+engine = pyttsx3.init()
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def get_voices(voice):
+    voices = engine.getProperty('voices')
+    if voice == 1:
+        engine.setProperty('voice', voices[1].id)
+        
+
+
+def time():
+    Time = datetime.datetime.now().strftime('%I:%M:%S')
+    speak("The current time is:")
+    speak(Time)
+
+
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    day = int(datetime.datetime.now().day)
+    speak("The current date is:")
+    speak(day)
+    speak(month)
+    speak(year)
+
+
+def greeting():
+    hour = datetime.datetime.now().hour
+    if hour >= 6 and hour <12:
+        speak("Good morning!")
+    elif hour >= 12 and hour <18: 
+        speak("Good afternoon!")
+    elif hour >= 18 and hour < 24:
+        speak("Good evening!")
+    else: 
+        speak("Good night!")
+
+
+def wish_me():
+    greeting()
+    time()
+    date()
+    speak("Maeve at your service. Is there something i can help you with?")
+
+
+def take_ommand_CMD():
+    query = input("Is there something i can help you with?\n")
+    return query
+
+
+def take_command_mic():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try: 
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en')
+        print(query)
+    except Exception as e:
+        print(e)
+        speak("Can you repeat that?")
+        print("Can you repeat that?...")
+        return "None"
+    return query
+
+
+def search_google():
+    speak('What should i search for?')
+    search = take_command_mic()
+    wb.open('https://www.google.com/search?q='+search)
+
+
+def news():
+    newsapi = NewsApiClient(api_key='7ed4eab6f966410188060a66982f2b3e')
+    speak('What topic are you interested in today?')
+    topic = take_command_mic()
+    data = newsapi.get_top_headlines(q=topic,
+                                    language='en',
+                                    page_size=5)
+    newsdata = data['articles']
+    for x,y in enumerate(newsdata):
+        print(f'{x}{y["description"]}')
+        speak((f'{x}{y["description"]}'))
+
+    speak("These were the most important news for now!")
+
+
+def text_to_speech():
+    text = clipboard.paste()
+    print(text)
+    speak(text)
+
+
+#name_img = tt.time()
+    #name_img = f'D:\\Github\\Screenshots\\{name_img}.png' #works for iva
+    #img = pyautogui.screenshot(name_img)
+    #img.show()
+
+
+if __name__ == "__main__":
+    get_voices(1)
+    #wish_me()
+    greeting()
+    
+    while True:
+        query = take_command_mic().lower()
+
+        if 'time' in query :
+            time()
+
+        elif 'date' in query:
+            date()
+
+        elif 'wikipedia' in query:
+            speak('Searching on Wikipedia...')
+            query = query.replace("wikipedia", "")
+            result = wikipedia.summary(query, sentences = 2)
+            print(result)
+            speak(result)
+
+        elif 'search' in query:
+            search_google()
+
+        elif 'youtube' in query:
+            #speak("What should i search for on YouTube?")
+            #topic = take_command_mic()
+            kit.playonyt("7 Rings song")
+            print("Playing...")
+
+        elif 'news' in query:
+            news()
+
+        elif 'read' in query:
+            text_to_speech()
+
+        elif 'open code' in query:
+            codepath = 'C:\\Users\\ivama\\.vscode'
+            os.startfile(codepath)
+
+        elif 'joke' in query:
+            joke = pyjokes.get_joke(language="en", category="all")
+            print(joke)
+            speak(joke)
+            
+        #elif 'screenshot' in query:
+            #screenshot()
+            #print("Screenshot taken")
+    
+        elif 'offline' in query:
+            quit()
+
+        else:
+            speak("Can you repeat that?")
+            print("Can you repeat that?")
+        
+        """elif 'remember':
+            speak("What should i remember?")
+            data = take_command_mic()
+            speak("remembered" +data)
+            remember = open('remembered_data.txt', 'w')
+            remember.write(data)
+            remember.close()
+
+        elif 'tell them what i told you':
+            remembered = open('remembered_data.txt', 'r')
+            speak("You told me that" +remembered.read())"""
